9d079677fcf535913090353e4b2074e7dbcc9a2a
Israq-sebpo <141021671+Israq-sebpo@users.noreply.github.com>
2024-10-02 20:08:31 +0600

    assignment.js

---
+++ assignment.js
@@ -0,0 +1,5 @@
+let country = "Bangladesh";
+let continent = "South Asia";
+let population = 20*100;
+
+console.log(country,continent);
---
+++ script.js
@@ -0,0 +1,13 @@
+// let js = 'amazing';
+// if(js === 'amazing') alert('right')
+//     else
+// alert ('wrong');
+// let define = 40+9+6+7;
+// console.log(define);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
