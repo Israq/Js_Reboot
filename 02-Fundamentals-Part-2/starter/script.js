707dab6006f8c598e8c24962473ffd81499f9e4c
Syed Ragib Israq <141021671+Israq-sebpo@users.noreply.github.com>
2025-04-13 19:29:36 +0600

    Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true
+  ? ((hasDriverLicense = true), console.log("I have license"))
+  : console.log("I can drive");
+
+const private = 534;
