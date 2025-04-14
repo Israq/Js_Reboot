53433af7509829c9bca8f5b370dce23cf624bed8
Israq <ragib.israq9833@gmail.com>
2025-04-14 13:08:49 +0600

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,8 +1,25 @@
 "use strict";
 
-let hasDriversLicense = false;
-const passTest = true
-  ? ((hasDriverLicense = true), console.log("I have license"))
-  : console.log("I can drive");
+// let hasDriversLicense = false;
+// const passTest = true
+//   ? ((hasDriverLicense = true), console.log("I have license"))
+//   : console.log("I can drive");
 
-const private = 534;
+// const private = 534;
+
+// const present = 2025;
+// const birth = 1996;
+
+// function logger() {
+//   console.log(`My age is ${present - birth} years old`);
+// }
+// // invoking the function
+// logger();
+
+function ageCalc(present, birth) {
+  console.log(2025, 1996);
+  const eligibility = `My age is now ${present - birth} years old`;
+  return eligibility;
+}
+
+console.log(ageCalc(2025, 1996));
