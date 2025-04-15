bfb601342db42221bebb3df77738b63892fb2026
Israq <ragib.israq9833@gmail.com>
2025-04-15 13:29:56 +0600

    Function complete fundatmaental-2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -16,6 +16,7 @@
 // // invoking the function
 // logger();
 
+//Function declaration
 function ageCalc(present, birth) {
   console.log(2025, 1996);
   const eligibility = `My age is now ${present - birth} years old`;
@@ -23,3 +24,45 @@ function ageCalc(present, birth) {
 }
 
 console.log(ageCalc(2025, 1996));
+
+//Function expression
+
+const ageCalc2 = function (present, birth) {
+  return present - birth;
+};
+
+const age2 = ageCalc2(2025, 1996);
+console.log(`In age2 I am ${age2} years old`);
+
+//Arrow function
+
+const ageCalc3 = (present, birth) => {
+  return present - birth;
+};
+
+const age3 = ageCalc3(2025, 1996);
+console.log(`At age3 my age is ${age3} years old`);
+
+const yearsUntilRetirement = (birthYear, currentYear) => {
+  const age = currentYear - birthYear;
+  const retirement = 65 - age;
+  return `Bob is ${age} years old and retires in ${retirement} years`;
+};
+console.log(yearsUntilRetirement(1991, 2025));
+
+//Function calling another function
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `For juice we need ${applePieces} pieces of apple and ${orangePieces} pieces of oranges`;
+
+  return juice;
+}
+
+console.log(fruitProcessor(4, 8));
